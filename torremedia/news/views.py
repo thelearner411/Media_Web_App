5ae069e28c57df0a39042e3dee718c5b594fde42
MC <mc@Miguels-MBP.home>
2022-12-31 01:29:25 +0100

    basic app setup

---
+++ torremedia/news/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Category
+from .models import Article
+
+
+def helloWorld(request):
+    return HttpResponse("Hello world!")
+
+def home(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+def news(request):
+    categories = Category.objects.all().values()
+    template = loader.get_template('categories.html')
+    context = {
+        'categories': categories,
+    }
+    return HttpResponse(template.render(context, request))
+
+def browse(request, id):
+    category = Category.objects.get(id=id)
+    template = loader.get_template('browse.html')
+    context = {
+        'category': category,
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'categories': ['Technology', 'Health', 'Culture', 'Politics', 'Sports', 'Entertainment'],   
+  }
+  return HttpResponse(template.render(context, request))
