8d41a4fdc7057c266ed78fc31db8ce18c3c414dc
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2023-10-13 10:29:33 +0200

    update footer year@

--- torremedia/static/script.js
+++ torremedia/static/script.js
@@ -1,6 +1,7 @@
 function loadDefaults(){
     let footerYear = document.getElementById("footer-year");
+    let startYear = 2022;
     let date = new Date();
     let currentYear = date.getFullYear();
-    footerYear.innerHTML = "© " + currentYear + " Torre Media. All Rights Reserved.";
+    footerYear.innerHTML = "© " + startYear + " - " + currentYear + " Torre Media. All Rights Reserved.";
 }
